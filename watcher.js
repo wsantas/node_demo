5fc43628e8df99218e9de1205e92d1a9f792c183
William Santasiero <wsantas@live.com>
2015-03-08 14:43:35 -0400

    event emitter exercise

--- watcher.js
+++ watcher.js
@@ -4,5 +4,5 @@
 const fs = require('fs');
 fs.watch('target.txt', function () {
     console.log("File 'target.txt' just changed");
-})                                                ;
+});
 console.log("Now watching target.txt for changes");
