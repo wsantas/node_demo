49e7bce0dfd4dfe1c54d8c3e619f83a85d2fe04b
William Santasiero <wsantas@live.com>
2015-04-05 10:36:57 -0400

    dockerizing the watcher to play with docker

--- file-system/watcher.js
+++ file-system/watcher.js
@@ -2,7 +2,7 @@
  * Created by wsantasiero on 3/7/15.
  */
 const fs = require('fs');
-fs.watch('target.txt', function () {
+fs.watch('file-system/target.txt', function () {
     console.log("File 'target.txt' just changed");
 });
 console.log("Now watching target.txt for changes");
